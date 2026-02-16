321eeda58d5474e0ac6ae409492510b82c439e85
m musa <musakhanoo2007@gmail.com>
2026-02-16 13:30:35 +0500

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>// preprocessor directive to include the input/output stream library
+using namespace std;//allows us to use names for objects/variables from the standard library (like cout)
+int main()//the main function where program execution begins 
+{//start of the main function block
+	int a = 1;//declares an integer variable 'a' and assigns it the value 1
+	int b = 2;//declares an integer variable 'b' and assigns it the value 2
+	cout<<a+b;//calculates the sum of a and b , then prints the result (3)to the console
+
+	
+	return 0;//signals that the program is finished succesfully
+}//end of the main function block
