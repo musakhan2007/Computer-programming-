2265e74d40e5b9ff8883bdddf37f6e7cc5210bc8
m musa <musakhanoo2007@gmail.com>
2026-03-09 10:38:35 +0500

    programs/lab1.cpp

---
+++ Task 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //include library for input and output
+using namespace std; // allow standard names like "cout and cin" without std:
+// function main begins program execution
+int main()// it is sttarting point of program
+    {
+
+
+ return 0; // program end kara hai
+}
