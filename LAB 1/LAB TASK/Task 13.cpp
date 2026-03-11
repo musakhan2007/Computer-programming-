034ea20c07c6c3d69facde6833295d0e70ebe2fd
m musa <musakhanoo2007@gmail.com>
2026-03-11 10:22:32 +0500

    Add files via upload

---
+++ LAB 1/LAB TASK/Task 13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    int a;
+    cout << "a = " << a;// for out put  display  and we enter the number in out put display
+    
+    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
