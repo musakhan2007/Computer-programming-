98fc79d425d5346f454869c35c1b4c1f3daa9149
m musa <musakhanoo2007@gmail.com>
2026-03-11 10:31:52 +0500

    Add files via upload

---
+++ LAB 3/LAB WORK/Work 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+int num1; // VARIABLE DECLERE KRNY K LYE
+
+cout<<"enter num\n";// DISPLAY K LYE 
+cin>>num1;  // USER JO INPUT DE GA 
+
+if(num1>0)//   CONDITION LAGANY K LYE 
+cout<<num1<<" is positive"<<endl;   // PRINT KRNY K LYE 
+
+if(num1<0)//   CONDITION LAGAANY K LYE 
+cout<<num1<<" is negative"<<endl;//   DISPLAY KARNY K LYE 
+return 0;
+}
