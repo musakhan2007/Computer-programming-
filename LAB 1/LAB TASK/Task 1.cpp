a225cf9525d882d24b27c49a1b3dcfb9bca32daf
m musa <musakhanoo2007@gmail.com>
2026-03-11 10:41:11 +0500

    Delete LAB 1/LAB TASK directory

--- LAB 1/LAB TASK/Task 1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> //include library for input and output
-using namespace std; // allow standard names like "cout and cin" without std:
-// function main begins program execution
-int main()// it is sttarting point of program
-    {
-
-
- return 0; // program end kara hai
-}
