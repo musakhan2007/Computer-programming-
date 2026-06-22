f3252b12ca281083430d630e1f3021cd6a78e60b
m musa <musakhanoo2007@gmail.com>
2026-06-22 15:54:32 +0500

    Add files via upload

---
+++ LAB 9/LAB TASK/Lab No 09 Task 1.cpp
@@ -0,0 +1,24 @@
+/*
+Name: Muhammad MUSA
+Registration No: BF25NWELE0754
+
+Department: Electrical
+Section: Power
+
+Program: Function Introduction and Declaration
+*/
+
+#include <iostream>
+using namespace std;
+
+void greet()      // Function declare kiya gaya hai jo koi parameter nahi leta
+{
+    cout << "Welcome to C++ Programming!" << endl;      // Greeting message display ho raha hai
+}
+
+int main()
+{
+    greet();      // Function call kiya gaya hai
+
+    return 0;      // Program successful end
+}
