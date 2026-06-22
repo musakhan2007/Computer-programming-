25793923441b199a5caeeef99d4c5b59b58c02a0
m musa <musakhanoo2007@gmail.com>
2026-06-22 16:02:56 +0500

    Add files via upload

---
+++ LAB 13/LAB TASK/Lab No 13 Task (2).cpp
@@ -0,0 +1,38 @@
+/*
+----------------------------------------------------
+Name: Muhammad MUSA
+Registration No: BF25NWELE0754
+Department: Electrical Engineering
+Section: Power
+PROGRAM TITLE: Structure Declaration and Initialization (Student)
+
+----------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+    string name;   // student ka naam store karne ke liye variable
+    int age;       // student ki age store karne ke liye variable
+    char grade;    // student ka grade store karne ke liye variable
+};
+
+int main()
+{
+    // Student type ka variable banaya gaya
+    Student student1;
+
+    // members ko values assign ki gayi hain
+    student1.name = "Muhammad MUSA";   // naam assign kiya
+    student1.age = 18;                    // age assign ki
+    student1.grade = 'A';                 // grade assign kiya
+
+    // structure ke values display ki ja rahi hain
+    cout << "Name: " << student1.name << endl;    // name print ho raha hai
+    cout << "Age: " << student1.age << endl;      // age print ho rahi hai
+    cout << "Grade: " << student1.grade << endl;  // grade print ho raha hai
+
+    return 0;  // program end
+}
